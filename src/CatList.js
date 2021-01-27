79fd5f454554e155d1bcaa69e57ee13ab4ae7c8c
snganyon@terpmail.umd.edu <snganyone1@gmail.com>
2021-01-27 10:19:44 -0800

    Done.

--- src/CatList.js
+++ src/CatList.js
@@ -1,1 +1,10 @@
 // write your CatList component here
+import React, { Component } from 'react';
+
+class CatList extends Component {
+    render(){
+        return(<div></div>)
+    }
+}
+
+export default CatList;
